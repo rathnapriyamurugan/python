c47b42271037ceb66f4daabfd98a823358af36c8
rathnapriyamurugan <36758480+rathnapriyamurugan@users.noreply.github.com>
2018-03-17 09:47:33 +0000

    Create primeinterval.py

---
+++ beginner level/primeinterval.py
@@ -0,0 +1,10 @@
+low = 100
+upp= 200
+print("Prime numbers between",low,"and",upp,"are:")
+for num in range(low,upp + 1):
+     if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
