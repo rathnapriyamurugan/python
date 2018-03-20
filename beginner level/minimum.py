15136c03ebcd0a2a1c5230f00377db16d1ca8dea
rathnapriyamurugan <36758480+rathnapriyamurugan@users.noreply.github.com>
2018-03-20 08:32:24 -0700

    Create minimum.py

---
+++ beginner level/minimum.py
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+cout<<"enter the numbers";
+cin>>a>>b>>c;
+if(a<b)&&(a<c)
+{
+cout<<"a is minimum";
+}
+elseif(b<c)
+{
+cout<<"b is minimum";
+}
+else
+{
+cout<<"c is minimum"
+}
+getch();
+}
