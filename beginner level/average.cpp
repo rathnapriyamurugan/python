b85affb67eb1cee2f1ac06df14ee6e049f014e01
rathnapriyamurugan <36758480+rathnapriyamurugan@users.noreply.github.com>
2018-03-21 07:49:38 -0700

    Create average.cpp

---
+++ beginner level/average.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+float a,b,c,sum,av;
+cout<<“Enter three numbers:”;
+cin>>a>>b>>c;
+sum=a+b+c;
+av=sum/3;
+cout<<“nSUM=”<<sum;
+cout<<“nAverage=”<<av;
+getch(); 
+}
+
