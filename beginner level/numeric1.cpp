4243efe457342ae89f2fdf092ac3d95870bd4b4f
rathnapriyamurugan <36758480+rathnapriyamurugan@users.noreply.github.com>
2018-03-23 07:06:22 -0700

    Create numeric1.cpp

---
+++ beginner level/numeric1.cpp
@@ -0,0 +1,31 @@
+#include <iostream.h>
+#include<conio.h>
+int isNumericString(unsigned char *num)
+{
+	int i=0;
+    while (*(num+i)) {
+        if (*(num+i) >= '0' && *(num+i) <= '9')
+            i++;
+        else
+            return 0;
+    }
+    return 1;
+}
+
+void main()
+{
+	int ret = 0;
+	unsigned char str1[] = "123";
+	unsigned char str2[] = "ABC";
+	ret = isNumericString(str1);
+	if(ret)
+		cout<<"It is numeric string"<<endl;
+	else
+		cout<<"It is not numeric string"<<endl;	
+	ret = isNumericString(str2);
+	if(ret)
+		cout<<"It is numeric string"<<endl;
+	else
+		cout<<"It is not numeric string"<<endl;		
+	getch();
+}
