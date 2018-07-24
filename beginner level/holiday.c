4854ee7f46527a26f2cdd0e19a78663377a9655e
rathnapriyamurugan <36758480+rathnapriyamurugan@users.noreply.github.com>
2018-07-24 10:02:46 +0530

    Create holiday.c

---
+++ beginner level/holiday.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the number");
+scanf("%d,&n);
+switch(n)
+{
+case 1:
+ {
+ printf("SUNDAY");
+ printf("HOLIDAY");
+ break;
+ }
+case 2:
+case 3:
+case 4:
+case 5:
+case 6:
+case 7:
+ {
+ printf("WORKING DAY");
+ breakk;
+ }
+}
+getch();
+}
